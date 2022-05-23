38811fd4bc23381cbcb37cf806d5522761dc2a10
Ingvaar <19124383+ingvaar@users.noreply.github.com>
2022-05-23 17:12:59 +0200

    feat: add base project sources

---
+++ src/main/kotlin/TopologyProducer.kt
@@ -0,0 +1,25 @@
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.Topology
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import javax.enterprise.context.ApplicationScoped
+import javax.enterprise.inject.Produces
+
+@ApplicationScoped
+class TopologyProducer {
+    @field:ConfigProperty(name = "topic.in", defaultValue = "topic.in")
+    lateinit var topicIn: String
+    @field:ConfigProperty(name = "topic.out", defaultValue = "topic.out")
+    lateinit var topicOut: String
+    
+    private val logger: Logger = LoggerFactory.getLogger(TopologyProducer::class.java)
+
+    @Produces
+    fun buildTopology(): Topology {
+
+        return StreamsBuilder().apply {
+            // Add topology here
+        }.build()
+    }
+}
